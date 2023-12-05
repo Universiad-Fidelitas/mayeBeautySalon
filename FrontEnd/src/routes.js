5a20e4723060f5d1912bdeaf6a728e2e75f50334
Jose Mauricio Granados Muñoz <mauriciogrm@Joses-MacBook-Air.local>
2023-12-04 20:49:31 -0600

    Agregando restante

--- FrontEnd/src/routes.js
+++ FrontEnd/src/routes.js
@@ -1,7 +1,13 @@
 import { DEFAULT_PATHS } from 'config.js';
 import Citas from 'views/admin/Citas/Citas';
 import Dashboard from 'views/admin/Dashboard';
+import Facturas from 'views/admin/Facturas/Facturas';
+import Transacciones from 'views/admin/Facturas/Transacciones';
+import Categorias from 'views/admin/Inventario/Categorias';
+import Inventario from 'views/admin/Inventario/Inventario';
 import Productos from 'views/admin/Inventario/Productos';
+import Proveedores from 'views/admin/Inventario/Proveedores';
+import Servicios from 'views/admin/Inventario/Servicios';
 import Roles from 'views/admin/Usuarios/Roles';
 import Usuarios from 'views/admin/Usuarios/Usuarios';
 
@@ -32,7 +38,7 @@ const routesAndMenuItems = {
     {
       path: `${appRoot}/trabajadores`,
       label: 'menu.trabajadores',
-      icon: 'tea',
+      icon: 'laptop',
       protected: true,
       subs: [
         { path: '/usuarios',icon: 'user', label: 'menu.users', component: Usuarios },
@@ -40,12 +46,26 @@ const routesAndMenuItems = {
       ],
     },
     {
-      path: `${appRoot}/inventario`,
-      label: 'Inventario',
-      icon: 'activity',
+      path: `${appRoot}/inventariado`,
+      label: 'Inventariado',
+      icon: 'database',
       protected: true,
       subs: [
-        { path: '/productos',icon: 'anchor', label: 'Productos', component: Productos },
+        { path: '/inventario',icon: 'file-text', label: 'Inventario', component: Inventario },
+        { path: '/categorias',icon: 'dropdown', label: 'Categorias', component: Categorias },
+        { path: '/productos',icon: 'gift', label: 'Productos', component: Productos },
+        { path: '/proveedores',icon: 'destination', label: 'Proveedores', component: Proveedores },
+        { path: '/servicios',icon: 'wizard', label: 'Servicios', component: Servicios },
+      ],
+    },
+    {
+      path: `${appRoot}/factuación`,
+      label: 'Factuación',
+      icon: 'book',
+      protected: true,
+      subs: [
+        { path: '/facturas',icon: 'content', label: 'Facturas', component: Facturas },
+        { path: '/transacciones',icon: 'book-open', label: 'Transacciones', component: Transacciones },
       ],
     },
   ],
