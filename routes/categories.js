cf265448e80c465358113c0617cf497cd64727ab
Jose Ramos <d.anielramos.ov@gmail.com>
2024-02-05 17:40:19 -0600

    Se crea y se prueba el backend de Categorias

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
-const { getCategories, postCategories } = require('../controllers/categoriesController');
+const { check, param } = require('express-validator');
+const { getCategories,
+    postCategory,
+    putCategory,
+    deleteCategory,
+    getById } = require('../controllers/categoriesController');
 const router = Router();
 
-router.get('/', getCategories)
+router.post('/', getCategories)
+router.post('/add', postCategory)
 
-router.post('/', postCategories)
+router.get('/:category_id', [ param('category_id').isNumeric().withMessage('category_id must be a number')], getById)
+router.put('/:category_id', [ param('category_id').isNumeric().withMessage('category_id must be a number')], router.put('/:category_id', [ param('category_id').isNumeric().withMessage('category_id must be a number')], putCategory)
+)
+router.post('/delete', [ param('category_id').isNumeric().withMessage('category_id must be a number')], deleteCategory)
 
 module.exports = router;
