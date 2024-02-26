15636d3e73d3c51a86d2262214fca384793fc1cf
MauricioGrMDS <mauricio.granados@datasite.com>
2024-02-25 22:17:40 -0600

    MATE-CT-2: Agregando progreso a citas

--- FrontEnd/src/views/publicViews/AppointmentsClient.js
+++ FrontEnd/src/views/publicViews/AppointmentsClient.js
@@ -1,40 +1,20 @@
 import React, { createRef, useState } from 'react';
 import HtmlHead from 'components/html-head/HtmlHead';
 import useCustomLayout from 'hooks/useCustomLayout';
-
-
 import { Wizard, Steps, Step, WithWizard } from 'react-albus';
 import { Button, Form, Spinner } from 'react-bootstrap';
 import { Formik, Field } from 'formik';
 import CsLineIcons from 'cs-line-icons/CsLineIcons';
-
-import { useGetAllServices } from 'hooks/react-query/useServices';
-
-import { useIntl } from 'react-intl';
 import { MENU_PLACEMENT } from '../../constants';
 import { FirstDataRequestTap } from './Appointments/FirstDataRequestTap';
+import { SecondDataRequestTap } from './Appointments/SecondDataRequestTap';
 
 export const AppointmentsClient = () => {
     const title = 'Horizontal Menu';
     const description = 'Horizontal standard menu that turns into mobile menu for smaller screens.'
-
-    
-
-
-
-
-
-
-
     const forms = [createRef(null), createRef(null)];
     const [bottomNavHidden, setBottomNavHidden] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [fields, setFields] = useState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-    });
   
     const onClickNext = (goToNext, steps, step) => {
       if (steps.length - 1 <= steps.indexOf(step)) {
@@ -45,18 +25,18 @@ export const AppointmentsClient = () => {
   
       form.submitForm().then(() => {
         if (!form.isDirty && form.isValid) {
-          const newFields = { ...fields, ...form.values };
-          setFields(newFields);
+          // const newFields = { ...fields, ...form.values };
+          // setFields(newFields);
   
-          if (steps.length - 2 <= steps.indexOf(step)) {
-            // done
-            setBottomNavHidden(true);
-            setLoading(true);
-            console.log(newFields);
-            setTimeout(() => {
-              setLoading(false);
-            }, 3000);
-          }
+          // if (steps.length - 2 <= steps.indexOf(step)) {
+          //   // done
+          //   setBottomNavHidden(true);
+          //   setLoading(true);
+          //   console.log(newFields);
+          //   setTimeout(() => {
+          //     setLoading(false);
+          //   }, 3000);
+          // }
           goToNext();
           step.isDone = true;
         }
@@ -110,45 +90,12 @@ export const AppointmentsClient = () => {
         />
         <Steps>
           <Step id="step1" name="Primer Paso" desc="Elegir el servicio">
-              <FirstDataRequestTap formRef={forms}/>
+          <FirstDataRequestTap formRef={forms}/>
+
           </Step>
-          <Step id="step2" name="Second" desc="Second description">
-            <div className="sh-30">
-              <Formik
-                innerRef={forms[1]}
-                initialValues={{
-                  email: fields.email,
-                  password: fields.password,
-                }}
-                validateOnMount
-                onSubmit={() => {}}
-              >
-                {({ errors, touched }) => (
-                  <Form>
-                    <h5 className="card-title">Second Title</h5>
-                    <p className="card-text text-alternate mb-4">Pastry wafer icing icing marshmallow dessert jelly-o apple pie lollipop.</p>
-                    <div className="mb-3 top-label tooltip-end-top">
-                      <Form.Label>EMAIL</Form.Label>
-                      <Field className="form-control" name="email" />
-                      {errors.email && touched.email ? (
-                        <Form.Control.Feedback type="invalid" tooltip className="d-block">
-                          {errors.email}
-                        </Form.Control.Feedback>
-                      ) : null}
-                    </div>
-                    <div className="mb-3 top-label tooltip-end-top">
-                      <Form.Label>PASSWORD</Form.Label>
-                      <Field className="form-control" type="password" name="password" />
-                      {errors.password && touched.password ? (
-                        <Form.Control.Feedback type="invalid" tooltip className="d-block">
-                          {errors.password}
-                        </Form.Control.Feedback>
-                      ) : null}
-                    </div>
-                  </Form>
-                )}
-              </Formik>
-            </div>
+          <Step id="step2" name="Segundo Paso" desc="Información del cliente">
+
+                          <SecondDataRequestTap formRef={forms} />
           </Step>
           <Step id="step3" hideTopNav>
             <div className="sh-30 d-flex flex-column justify-content-center align-items-center">
