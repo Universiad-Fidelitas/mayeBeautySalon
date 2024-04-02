0d201db5481dbfdbbb0ddcd4d6d11d2f957a9515
Jose Ramos <d.anielramos.ov@gmail.com>
2024-04-01 20:46:07 -0600

    Se hace validaciones y queda pendiente el search user, todo esto se hace sin Karo :,v

--- FrontEnd/src/views/admin/Facturas/ModalAddEditFacturas.js
+++ FrontEnd/src/views/admin/Facturas/ModalAddEditFacturas.js
@@ -4,10 +4,13 @@ import { Formik, Field, Form, FieldArray, ErrorMessage } from 'formik';
 import 'react-dropzone-uploader/dist/styles.css';
 import { useStock } from 'hooks/react-query/useStock';
 import Select from 'react-select';
+import { toast } from 'react-toastify';
+import { useIntl } from 'react-intl';
 
 export const ModalAddEditFacturas = ({ tableInstance, addItem, editItem, validationSchema, formFields }) => {
   const { selectedFlatRows, setIsOpenAddEditModal, isOpenAddEditModal } = tableInstance;
   const { data: productsData } = useStock();
+  const { formatMessage: f } = useIntl();
   const productsDataDropdown = useMemo(
     () =>
       productsData?.items.map(({ product_id, name, total_amount }) => {
@@ -49,16 +52,15 @@ export const ModalAddEditFacturas = ({ tableInstance, addItem, editItem, validat
     if (values.dataToInsert[0].invetory_products_id === '') {
       values.dataToInsert[0].invetory_products_id = 0;
     }
+
     values.dataToInsert.forEach((item, index) => {
-      if (values.action === 'remove') {
-        const product = productsDataDropdown.find((p) => p.value === item.product_id);
-        console.log('product', product);
-        if (product && Number(product.amount) < item.amount) {
-          isValid = false;
-          // ARREGLAR ESTO USAR UN TOAST
-          console.log(index);
-          // alert(`El producto en la posición ${index + 1} no tiene suficiente cantidad para remover.`);
-        }
+      const product = productsDataDropdown.find((p) => p.value === item.product_id);
+      console.log('product', product);
+      if (product && Number(product.amount) < item.amount) {
+        isValid = false;
+        // ARREGLAR ESTO USAR UN TOAST
+        console.log(index);
+        toast(f({ id: `El producto en la posición ${index + 1} no tiene suficiente cantidad para remover.` }), { className: 'danger' });
       }
     });
 
@@ -106,36 +108,25 @@ export const ModalAddEditFacturas = ({ tableInstance, addItem, editItem, validat
             <Modal.Body>
               <div className="mb-3">
                 <label className="form-label">Estado del Pago</label>
-                <Field className="form-control" name="status" id="status" component={CustomSelect} options={optionsStatus} />
-                <ErrorMessage name="status" component="div" className="field-error" />
+                <Field className="form-control" name="status" id="status" component={CustomSelect} options={optionsStatus} required />
+                <ErrorMessage style={{ color: 'red' }} name="status" component="div" className="field-error" />
               </div>
               <div className="mb-3">
                 <label className="form-label">Tipo de Pago</label>
-                <Field className="form-control" name="payment_type" id="payment_type" component={CustomSelect} options={optionsPayment} />
-                <ErrorMessage name="payment_type" component="div" className="field-error" />
+                <Field className="form-control" name="payment_type" id="payment_type" component={CustomSelect} options={optionsPayment} required />
+                <ErrorMessage style={{ color: 'red' }} name="payment_type" component="div" className="field-error" />
               </div>
               <div className="mb-3" key="description">
                 <label className="form-label">Descripción</label>
                 <Field as="textarea" className="form-control" type="text" id="description" name="description" />
-                <ErrorMessage name="description" component="div" />
-              </div>
-
-              <div className="mb-3" key="payment_id">
-                <label className="form-label">ID del pago</label>
-                <Field className="form-control" type="text" id="payment_id" name="payment_id" disabled value={values.payment_id || '0'} />
-                <ErrorMessage name="payment_id" component="div" />
-              </div>
-              <div className="mb-3" key="inventory_id">
-                <label className="form-label">ID de la venta</label>
-                <Field className="form-control" type="text" id="inventory_id" name="inventory_id" disabled value={values.inventory_id || '0'} />
-                <ErrorMessage name="inventory_id" component="div" />
+                <ErrorMessage style={{ color: 'red' }} name="description" component="div" />
               </div>
 
               {formFields.map(({ id, label, type }) => (
                 <div className="mb-3" key={id}>
                   <label className="form-label">{label}</label>
                   <Field className="form-control" type={type} id={id} name={id} />
-                  <ErrorMessage name={id} component="div" />
+                  <ErrorMessage style={{ color: 'red' }} name={id} component="div" />
                 </div>
               ))}
 
@@ -157,33 +148,19 @@ export const ModalAddEditFacturas = ({ tableInstance, addItem, editItem, validat
                                 id="product_id"
                                 component={CustomSelect}
                                 options={productsDataDropdown}
+                                required
                               />
-                              <ErrorMessage name={`dataToInsert.${index}.product_id`} className="field-error" component="div" />
+                              <ErrorMessage style={{ color: 'red' }} name={`dataToInsert.${index}.product_id`} className="field-error" component="div" />
                             </div>
                           </>
                         )}
-                        <div className="mb-3" key={`dataToInsert.${index}.invetory_products_id`}>
-                          <label className="form-label">ID del IP</label>
-                          <Field
-                            className="form-control"
-                            type="text"
-                            id={`dataToInsert.${index}.invetory_products_id`}
-                            name={`dataToInsert.${index}.invetory_products_id`}
-                            disabled
-                            value={
-                              values.dataToInsert && values.dataToInsert[index] && values.dataToInsert[index].invetory_products_id
-                                ? values.dataToInsert[index].invetory_products_id
-                                : '0'
-                            }
-                          />
-                          <ErrorMessage name={`dataToInsert.${index}.invetory_products_id`} component="div" />
-                        </div>
+
                         <div className="mb-3">
                           <label className="form-label" htmlFor={`dataToInsert.${index}.amount`}>
                             Cantidad
                           </label>
                           <Field name={`dataToInsert.${index}.amount`} className="form-control" type="number" id="amount" />
-                          <ErrorMessage name={`dataToInsert.${index}.amount`} component="div" className="field-error" />
+                          <ErrorMessage style={{ color: 'red' }} name={`dataToInsert.${index}.amount`} component="div" className="field-error" />
                         </div>
                         {index > 0 && (
                           <div className="mb-3">
